22ce68d7237e2f656af2a88fa55d9b3cae76ce49
pms131 <sportssk@naver.com>
2017-06-08 21:28:01 +0900

    ver 1.11

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
-var install = require("gulp-install");
- 
-gulp.src(['./bower.json', './package.json'])
-  .pipe(install());
+var gulp = require('gulp');
+var del = require('del');
+var push = require('git-push');
+var argv = require('minimist')(process.argv.slice(2));
+
+gulp.task('clean', del.bind(null, ['build/*', '!build/.git'], {dot: true}));
+
+gulp.task('build', ['clean'], function() {
+  // TODO: Build website from source files into the `./build` folder
+});
+
+gulp.task('deploy', function(cb) {
+  var remote = argv.production ?
+    {name: 'production', url: 'https://github.com/<org>/site.com', branch: 'gh-pages'},
+    {name: 'test', url: 'https://github.com/<org>/test.site.com', branch: 'gh-pages'};
+  push('./build', remote, cb);
+});
