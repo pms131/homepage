60114bbe7d90b90a0e85e583f1e64db83af66fd6
pms131 <sportssk@naver.com>
2017-06-08 20:47:16 +0900

    v1.4

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,4 @@
-var gulp    = require('gulp'),
-      gutil    = require('gulp-util'),
-      uglify  = require('gulp-uglify'),
-    concat  = require('gulp-concat');
-
-gulp.task('js', function () {
-    gulp.src('./js/*.js')
-        .pipe(uglify())
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./js'));
-});
-
-gulp.task('default', function(){
-    gulp.run('js'); 
-});
-
-gulp.watch('./js/*', function () {
-     gulp.run('js');
-});
+var install = require("gulp-install");
+ 
+gulp.src(['./bower.json', './package.json'])
+  .pipe(install());
