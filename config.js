da058127fe67c9461032e0e4a5d6e7c4678786bf
pms131 <sportssk@naver.com>
2017-06-09 12:06:55 +0900

    v3.5

--- config.js
+++ config.js
@@ -1,13 +1,33 @@
 module.exports = {
-	server_port: 3000,
-	db_url: 'mongodb://admin:admin@databases-shard-00-00-oroo1.mongodb.net:27017,databases-shard-00-01-oroo1.mongodb.net:27017,databases-shard-00-02-oroo1.mongodb.net:27017/databases?ssl=true&replicaSet=databases-shard-0&authSource=admin',
-	db_schemas: [
-	    {file:'./user_schema', collection:'users', schemaName:'UserSchema', modelName:'UserModel'}
+    server_port: 3000,
+    db_url: 'mongodb://admin:admin@databases-shard-00-00-oroo1.mongodb.net:27017,databases-shard-00-01-oroo1.mongodb.net:27017,databases-shard-00-02-oroo1.mongodb.net:27017/databases?ssl=true&replicaSet=databases-shard-0&authSource=admin',
+    db_schemas: [
+        {
+            file: './user_schema',
+            collection: 'users',
+            schemaName: 'UserSchema',
+            modelName: 'UserModel'
+        }
 	],
-	route_info: [
+    route_info: [
 	    //===== User =====//
-	    {file:'./user', path:'/process/login', method:'login', type:'post'}					// user.login 
-	    ,{file:'./user', path:'/process/adduser', method:'adduser', type:'post'}				// user.adduser 
-	    ,{file:'./user', path:'/process/listuser', method:'listuser', type:'post'}			// user.listuser 
+        {
+            file: './user',
+            path: '/process/login',
+            method: 'login',
+            type: 'post'
+        } // user.login 
+	    , {
+            file: './user',
+            path: '/process/adduser',
+            method: 'adduser',
+            type: 'post'
+        } // user.adduser 
+	    , {
+            file: './user',
+            path: '/process/listuser',
+            method: 'listuser',
+            type: 'post'
+        } // user.listuser 
 	]
 }
--- database/user_schema.js
+++ database/user_schema.js
@@ -9,67 +9,77 @@ var crypto = require('crypto');
 
 var Schema = {};
 
-Schema.createSchema = function(mongoose) {
-	
-	// 스키마 정의
-	var UserSchema = mongoose.Schema({
-	    id: {type: String, required: true, unique: true, 'default':''},
-	    password: {type: String, required: true, 'default':''},
-	});
-	
-	
-	
-	// 인증 메소드 - 입력된 비밀번호와 비교 (true/false 리턴)
-	UserSchema.method('authenticate', function(plainText, inSalt, _password) {
-		if (inSalt) {
-			console.log('authenticate 호출됨 : %s -> %s : %s', plainText, this._password);
-			return this._password === password;
-		} else {
-			console.log('authenticate 호출됨 : %s -> %s : %s', plainText, this._password);
-			return this._password;
-		}
-	});
-	
-	// 값이 유효한지 확인하는 함수 정의
-	var validatePresenceOf = function(value) {
-		return value && value.length;
-	};
-		
-	// 저장 시의 트리거 함수 정의 (password 필드가 유효하지 않으면 에러 발생)
-	UserSchema.pre('save', function(next) {
-		if (!this.isNew) return next();
-	
-		if (!validatePresenceOf(this.password)) {
-			next(new Error('유효하지 않은 password 필드입니다.'));
-		} else {
-			next();
-		}
-	})
-	
-	// 필수 속성에 대한 유효성 확인 (길이값 체크)
-	UserSchema.path('id').validate(function (id) {
-		return id.length;
-	}, 'id 칼럼의 값이 없습니다.');
-	
-	UserSchema.path('password').validate(function (password) {
-		return password.length;
-	}, 'password 칼럼의 값이 없습니다.');
-	
-	   
-	// 스키마에 static 메소드 추가
-	UserSchema.static('findById', function(id, callback) {
-		return this.find({id:id}, callback);
-	});
-	
-	UserSchema.static('findAll', function(callback) {
-		return this.find({}, callback);
-	});
-	
-	console.log('UserSchema 정의함.');
+Schema.createSchema = function (mongoose) {
 
-	return UserSchema;
+    // 스키마 정의
+    var UserSchema = mongoose.Schema({
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+            'default': ''
+        },
+        password: {
+            type: String,
+            required: true,
+            'default': ''
+        },
+    });
+
+
+
+    // 인증 메소드 - 입력된 비밀번호와 비교 (true/false 리턴)
+    UserSchema.method('authenticate', function (plainText, inSalt, _password) {
+        if (inSalt) {
+            console.log('authenticate 호출됨 : %s -> %s : %s', plainText, this._password);
+            return this._password === password;
+        } else {
+            console.log('authenticate 호출됨 : %s -> %s : %s', plainText, this._password);
+            return this._password;
+        }
+    });
+
+    // 값이 유효한지 확인하는 함수 정의
+    var validatePresenceOf = function (value) {
+        return value && value.length;
+    };
+
+    // 저장 시의 트리거 함수 정의 (password 필드가 유효하지 않으면 에러 발생)
+    UserSchema.pre('save', function (next) {
+        if (!this.isNew) return next();
+
+        if (!validatePresenceOf(this.password)) {
+            next(new Error('유효하지 않은 password 필드입니다.'));
+        } else {
+            next();
+        }
+    })
+
+    // 필수 속성에 대한 유효성 확인 (길이값 체크)
+    UserSchema.path('id').validate(function (id) {
+        return id.length;
+    }, 'id 칼럼의 값이 없습니다.');
+
+    UserSchema.path('password').validate(function (password) {
+        return password.length;
+    }, 'password 칼럼의 값이 없습니다.');
+
+
+    // 스키마에 static 메소드 추가
+    UserSchema.static('findById', function (id, callback) {
+        return this.find({
+            id: id
+        }, callback);
+    });
+
+    UserSchema.static('findAll', function (callback) {
+        return this.find({}, callback);
+    });
+
+    console.log('UserSchema 정의함.');
+
+    return UserSchema;
 };
 
 // module.exports에 UserSchema 객체 직접 할당
 module.exports = Schema;
-
